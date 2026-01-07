8f4d53bb40fb07c3dc90b1e2660db843bd8b2f45
MasieJr <dev@masieseremu.co.za>
2026-01-08 00:19:55 +0200

    test

--- film_processing/lib/main.dart
+++ film_processing/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello world',
+      title: 'Hello world1',
       theme: ThemeData(
         // This is the theme of your application.
         //
