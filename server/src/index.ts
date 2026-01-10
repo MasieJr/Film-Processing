cd200d5e1dbc1cabae9c453ea1c81abd30219ffc
MasieJr <dev@masieseremu.co.za>
2026-01-10 18:29:27 +0200

    endpoints to get salesperson

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import branchRoutes from './routes/branchRoutes.js';
 import serviceRoutes from './routes/serviceRoutes.js';
+import salesPersonRoutes from './routes/salePersonRoutes.js';
 
 dotenv.config();
 
@@ -14,7 +15,8 @@ app.use(cors()); // Allow the Flutter app to talk to us
 app.use(express.json()); // Allow us to read JSON data
 
 app.use('/branches', branchRoutes);
-app.use('/services',serviceRoutes);
+app.use('/services', serviceRoutes);
+app.use('/salesperson', salesPersonRoutes);
 
 // A Simple Test Route
 app.get('/', (req, res) => {
