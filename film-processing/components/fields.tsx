609dab093b3a2c15c9b1b6dab29c42ea6dc8c0ee
MasieJr <dev@masieseremu.co.za>
2026-01-19 22:43:00 +0200

    component combining text and textfield into one

---
+++ film-processing/components/fields.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+import { ThemedText } from "./themed-text";
+import { ThemedTextInput } from "./themed-textinput";
+
+type ItemProps = {
+  label: string;
+  placeholder: string;
+};
+
+export default function fieldItem({ label, placeholder }: ItemProps) {
+  return (
+    <>
+      <ThemedText>{label}</ThemedText>
+      <ThemedTextInput
+        darkColor="#FFFFFF"
+        placeholder={placeholder}
+        style={styles.textInput}
+      />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  textInput: {
+    height: 40,
+    padding: 5,
+    borderWidth: 1,
+    borderColor: "#41B544",
+    backgroundColor: "#2c2c2c",
+  },
+});
