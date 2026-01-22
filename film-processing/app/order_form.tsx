e5bdf8cde841b2bbb1b205fa316161aa5ad937ce
MasieJr <141233418+MasieJr@users.noreply.github.com>
2026-01-22 17:49:14 +0200

    Update order_form.tsx

--- film-processing/app/order_form.tsx
+++ film-processing/app/order_form.tsx
@@ -3,7 +3,13 @@ import { ThemedText } from "@/components/themed-text";
 import { ThemedTextInput } from "@/components/themed-textinput";
 import { ThemedView } from "@/components/themed-view";
 import { useEffect, useState } from "react";
-import { Button, ScrollView, StyleSheet, TouchableOpacity, View } from "react-native";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { SalesPersonModal } from "@/components/salesPersonList";
@@ -11,30 +17,60 @@ import { Radiobutton } from "@/components/radiobutton";
 import { Radiogroup } from "@/components/radiogroup";
 
 export default function OrderFormScreen() {
-  const [count, setCount] = useState(1);
+  const [selectedPrice, setSelectedPrice] = useState(238);
   const [salesPerson, setSalesPerson] = useState("Please Select Sales Person");
   const [modalVisible, setModalVisible] = useState(false);
+  const [selected, setSelected] = useState(0);
+  const services = [
+    "Email in High Resolution",
+    "Email in low Resolution",
+    "Print Only",
+    "Print and email",
+    "Develop only",
+  ];
+  const prices = [238, 218, 313, 313, 89];
+
+  function select(index: number) {
+    setSelected(index);
+    setForm((prev) => ({
+      ...prev,
+      price: prev.quantity * prices[index],
+    }));
+    setSelectedPrice(prices[index]);
+  }
   const [form, setForm] = useState({
     name: "",
     email: "",
     phone: "",
     quantity: 1,
-    salesPerson:""
+    salesPerson: "",
+    selectedService: "",
+    price: 238,
   });
 
-  useEffect(() => {}, [count]);
+  useEffect(() => {}, [form]);
+
   const decrement = () => {
-    setCount((prev) => Math.max(1, prev - 1));
-    setForm({
-      ...form,
-      quantity: count,
+    setForm((prev) => {
+      const newQuantity = Math.max(1, prev.quantity - 1);
+
+      return {
+        ...prev,
+        quantity: newQuantity,
+        price: newQuantity * selectedPrice,
+      };
     });
   };
+
   const increment = () => {
-    setCount((prev) => prev + 1);
-    setForm({
-      ...form,
-      quantity: count,
+   setForm((prev) => {
+      const newQuantity = prev.quantity + 1;
+
+      return {
+        ...prev, 
+        quantity: newQuantity,
+        price: newQuantity * selectedPrice, 
+      };
     });
   };
 
@@ -47,112 +83,140 @@ export default function OrderFormScreen() {
 
   const handleSubmit = () => {
     console.log("Submitting Order:", form);
+    alert("Order Placed ");
   };
-
-  const openModal = () => {};
 
   const handleModalSubmit = (selectedPerson: string) => {
     setSalesPerson(selectedPerson);
     setForm({
       ...form,
-      salesPerson:selectedPerson,
-    })
+      salesPerson: selectedPerson,
+    });
     console.log("Modal returned:", selectedPerson);
   };
 
   return (
     <SafeAreaProvider style={{ flex: 1, justifyContent: "center" }}>
       <ThemedView style={{ flex: 1, padding: 20 }}>
-      <ScrollView>
-        <Textfield
-          label="Name & Surname"
-          placeholder="Enter your name and surname"
-          autoComplete="name"
-          onChangeText={(text) => handleInputChange("name", text)}
-        />
-        <Textfield
-          label="Email Address"
-          placeholder="Enter your email address"
-          autoComplete="email"
-          onChangeText={(text) => handleInputChange("email", text)}
-        />
-        <Textfield
-          label="Phone Number"
-          placeholder="Enter your phone number"
-          autoComplete="tel"
-          onChangeText={(text) => handleInputChange("phone", text)}
-        />
-        <View style={styles.stepper}>
-          <ThemedText style={styles.label}>Quantity</ThemedText>
-          <View style={{ flexDirection: "row", marginBottom: 10 }}>
-            <TouchableOpacity style={styles.stepperbtn} onPress={decrement}>
-              <ThemedView
-                lightColor="#F3F4F6"
-                darkColor="#2c2c2c"
-                style={styles.stepperbtn}
+        <ScrollView keyboardDismissMode="on-drag">
+          <Textfield
+            label="Name & Surname"
+            placeholder="Enter your name and surname"
+            autoComplete="name"
+            onChangeText={(text) => handleInputChange("name", text)}
+          />
+          <Textfield
+            label="Email Address"
+            placeholder="Enter your email address"
+            autoComplete="email"
+            onChangeText={(text) => handleInputChange("email", text)}
+          />
+          <Textfield
+            label="Phone Number"
+            placeholder="Enter your phone number"
+            autoComplete="tel"
+            onChangeText={(text) => handleInputChange("phone", text)}
+          />
+          <View style={styles.stepper}>
+            <ThemedText style={styles.label}>Quantity</ThemedText>
+            <View style={{ flexDirection: "row", marginBottom: 10 }}>
+              <TouchableOpacity style={styles.stepperbtn} onPress={decrement}>
+                <ThemedView
+                  lightColor="#F3F4F6"
+                  darkColor="#2c2c2c"
+                  style={styles.stepperbtn}
+                >
+                  <ThemedText>-</ThemedText>
+                </ThemedView>
+              </TouchableOpacity>
+              <ThemedTextInput
+                style={{ marginHorizontal: 10, fontSize: 20 }}
+                onChangeText={(text) => {
+                  handleInputChange("quantity", text);
+                }}
               >
-                <ThemedText>-</ThemedText>
-              </ThemedView>
-            </TouchableOpacity>
-            <ThemedTextInput
-              style={{ marginHorizontal: 10, fontSize: 20 }}
-              onChangeText={(text) => {
-                handleInputChange("quantity", text);
+                {form.quantity}
+              </ThemedTextInput>
+              <TouchableOpacity onPress={increment}>
+                <ThemedView
+                  lightColor="#F3F4F6"
+                  darkColor="#2c2c2c"
+                  style={styles.stepperbtn}
+                >
+                  <ThemedText lightColor="">+</ThemedText>
+                </ThemedView>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <ThemedText style={styles.label}>Sales Person</ThemedText>
+          <TouchableOpacity onPress={() => setModalVisible(true)}>
+            <ThemedView
+              darkColor="#2C2C2E"
+              lightColor="#F3F4F6"
+              style={styles.dropDown}
+            >
+              <ThemedText style={styles.label}>{salesPerson}</ThemedText>
+              <FontAwesome
+                style={{ color: "#9CA3AF" }}
+                name="chevron-down"
+                size={24}
+                color="black"
+              />
+            </ThemedView>
+          </TouchableOpacity>
+          <SalesPersonModal
+            visible={modalVisible}
+            onClose={() => setModalVisible(false)}
+            onSubmit={handleModalSubmit} // Pass the function down
+          />
+          <ThemedText style={styles.label}>Service</ThemedText>
+          {services.map((service, index) => (
+            <TouchableOpacity
+              key={index}
+              onPress={() => {
+                select(index);
               }}
             >
-              {count}
-            </ThemedTextInput>
-            <TouchableOpacity onPress={increment}>
-              <ThemedView
-                lightColor="#F3F4F6"
-                darkColor="#2c2c2c"
-                style={styles.stepperbtn}
-              >
-                <ThemedText lightColor="">+</ThemedText>
-              </ThemedView>
+              <Radiobutton
+                darkColor="#fff"
+                lightColor="#2c2c2c"
+                value={service}
+                isSelected={selected === index}
+              />
             </TouchableOpacity>
+          ))}
+        </ScrollView>
+        <ThemedView style={styles.bottomRow}>
+          <View style={{flexDirection:"row"}}>
+          <ThemedText style={styles.price}>Total: </ThemedText>
+          <ThemedText style={[styles.price, {color:"red"}]}>R{form.price}</ThemedText>
           </View>
-        </View>
-        <ThemedText style={styles.label}>Sales Person</ThemedText>
-        <TouchableOpacity onPress={() => setModalVisible(true)}>
-          <ThemedView
-            darkColor="#2C2C2E"
-            lightColor="#F3F4F6"
-            style={styles.dropDown}
-          >
-            <ThemedText style={styles.label}>{salesPerson}</ThemedText>
-            <FontAwesome
-              style={{ color: "#9CA3AF" }}
-              name="chevron-down"
-              size={24}
-              color="black"
-            />
-          </ThemedView>
-        </TouchableOpacity>
-        <SalesPersonModal
-          visible={modalVisible}
-          onClose={() => setModalVisible(false)}
-          onSubmit={handleModalSubmit} // Pass the function down
-        />
-        <ThemedText style={styles.label}>Service</ThemedText>
-        <Radiogroup />
-        <TouchableOpacity style={styles.submitbtn} onPress={handleSubmit}>
-          <ThemedText style={styles.btnText}>SUBMIT ORDER</ThemedText>
-        </TouchableOpacity>
-      </ScrollView>
+          <TouchableOpacity style={styles.submitbtn} onPress={handleSubmit}>
+            <ThemedText style={styles.btnText}>SUBMIT ORDER</ThemedText>
+          </TouchableOpacity>
+        </ThemedView>
       </ThemedView>
     </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
+  price:{
+    textAlign:"center",
+    fontSize:25,
+    fontWeight:"bold"
+  },
+  bottomRow:{
+    flexDirection: "row",
+    justifyContent:"space-between",
+    alignItems: "center"
+    
+  },
   stepper: {
     flexDirection: "row",
     justifyContent: "space-between",
   },
-  stepperText:{
-
-  },
+  stepperText: {},
   stepperbtn: {
     width: 25,
     height: 25,
@@ -161,18 +225,18 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
   submitbtn: {
-    width: "100%",
+    width: "55%",
     height: 45,
     justifyContent: "center",
     alignContent: "center",
     backgroundColor: "#41B544",
     borderRadius: 15,
+    padding:5
   },
   btnText: {
     textAlign: "center",
-    fontSize: 30,
+    fontSize: 20,
     fontWeight: "bold",
-    lineHeight: 40,
   },
   label: {
     fontSize: 20,
