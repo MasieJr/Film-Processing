244d8060d211d6511c0548619be8a65f0d1e7425
MasieJr <141233418+MasieJr@users.noreply.github.com>
2026-04-17 12:42:19 +0200

    added sizes

--- my-web-app/src/app/order_form/page.tsx
+++ my-web-app/src/app/order_form/page.tsx
@@ -10,10 +10,19 @@ export default function OrderFormPage() {
     "Print and email",
     "Develop only",
   ];
+
+  const printsSizes = [
+    "9x13 cm",
+    "10x15 cm",
+    "13x18 cm",
+    "15x20 cm"
+  ];
+  const printPrices = [445, 329, 895, 931];
   const prices = [260, 240, 329, 329, 95];
 
   const [selectedPrice, setSelectedPrice] = useState(260);
   const [selectedServiceIndex, setSelectedServiceIndex] = useState(0);
+  const [selectedSizeIndex, setSelectedSizeIndex] = useState(0);
 
   const [form, setForm] = useState({
     name: "",
@@ -22,6 +31,7 @@ export default function OrderFormPage() {
     quantity: 1,
     salesPerson: "",
     selectedService: services[0], 
+    selectedSize: printsSizes[1],
     price: 260,
     keepNegatives: false,
   });
@@ -39,6 +49,17 @@ export default function OrderFormPage() {
       price: prev.quantity * prices[index],
     }));
   };
+
+  const selectSize = (index:number) => {
+    setSelectedSizeIndex(index);
+    setSelectedPrice(printPrices[index]);
+    setForm((prev) => ({
+      ...prev,
+      selectedSize: printsSizes[index],
+      price: prev.quantity * printPrices[index],
+    }))
+
+  }
 
   const decrement = () => {
     setForm((prev) => {
@@ -107,6 +128,7 @@ export default function OrderFormPage() {
           />
         </div>
 
+
         {/* --- Quantity Stepper --- */}
         <div className="flex flex-row justify-between items-center">
           <label className="text-xl font-medium">Quantity</label>
@@ -154,7 +176,6 @@ export default function OrderFormPage() {
                   onClick={() => selectService(index)}
                   className="flex items-center space-x-3 text-left w-full"
                 >
-                  {/* Custom Radio Circle */}
                   <div className={`w-6 h-6 rounded-full border-2 flex items-center justify-center
                     ${isSelected ? 'border-[#41B544]' : 'border-gray-400'}
                   `}>
@@ -167,34 +188,55 @@ export default function OrderFormPage() {
           </div>
         </div>
 
-        {(form.selectedService === "Print Only" || form.selectedService === "Develop only") && (
-          <div className="flex flex-col mt-4 bg-gray-50 dark:bg-[#1a1a1a] p-4 rounded-xl border border-gray-200 dark:border-gray-800">
-            <label className="text-xl mb-3 font-medium">Keep Negatives?</label>
-            <div className="flex flex-row space-x-8">
-              
-              <button onClick={() => handleInputChange("keepNegatives", true)} className="flex items-center space-x-2">
+        {/* Prints Sizes */}
+        {(form.selectedService === "Print Only" || form.selectedService === "Print and email") && (
+        <div className="flex flex-col">
+          <label>Print Sizes</label>
+           <div className="flex flex-col space-y-3">
+            {printsSizes.map((size, index) => {
+              const isSelected = selectedSizeIndex === index;
+              return (
+                <button
+                  key={index}
+                  onClick={() => selectSize(index)}
+                  className="flex items-center space-x-3 text-left w-full"
+                >
+                  <div className={`w-6 h-6 rounded-full border-2 flex items-center justify-center
+                    ${isSelected ? 'border-[#41B544]' : 'border-gray-400'}
+                  `}>
+                    {isSelected && <div className="w-3 h-3 rounded-full bg-[#41B544]" />}
+                  </div>
+                  <span className="text-lg">{size}</span>
+                </button>
+              );
+            })}
+          </div>
+          </div>
+        )}
+
+        <hr className="border-[#41B544]"/>
+
+        {/* --- Keep Negatives --- */}
+        <div className="flex flex-col">
+          <label className="text-xl mb-1 font-medium">Keep Negatives</label>
+          <div className="flex flex-row space-x-8">
+           <button onClick={() => handleInputChange("keepNegatives", true)} className="flex items-center space-x-2">
                 <div className={`w-6 h-6 rounded-full border-2 flex items-center justify-center ${form.keepNegatives ? 'border-[#41B544]' : 'border-gray-400'}`}>
                   {form.keepNegatives && <div className="w-3 h-3 rounded-full bg-[#41B544]" />}
                 </div>
                 <span className="text-lg">Yes</span>
-              </button>
-
-              <button onClick={() => handleInputChange("keepNegatives", false)} className="flex items-center space-x-2">
-                <div className={`w-6 h-6 rounded-full border-2 flex items-center justify-center ${!form.keepNegatives ? 'border-[#41B544]' : 'border-gray-400'}`}>
-                  {!form.keepNegatives && <div className="w-3 h-3 rounded-full bg-[#41B544]" />}
-                </div>
-                <span className="text-lg">No</span>
-              </button>
-
-            </div>
+            </button>
+            <button onClick={() => handleInputChange("keepNegatives", false)} className="flex items-center space-x-2">
+              <div className={`w-6 h-6 rounded-full border-2 flex items-center justify-center ${!form.keepNegatives ? 'border-[#41B544]' : 'border-gray-400'}`}>
+                {!form.keepNegatives && <div className="w-3 h-3 rounded-full bg-[#41B544]" />}
+              </div>
+              <span className="text-lg">No</span>
+            </button>
           </div>
-        )}
-
+        </div>
       </div>
 
-      {/* --- Fixed Bottom Submit Bar --- */}
-      {/* This hovers just above your global App Shell bottom navigation */}
-      <div className="fixed bottom left-0 right-0 bg-white dark:bg-[#1a1a1a] border-t border-gray-300 dark:border-gray-800 p-4 shadow-lg">
+      <div className="fixed bottom-0 left-0 right-0 bg-white dark:bg-[#1a1a1a] border-t border-gray-300 dark:border-gray-800 p-4 shadow-lg">
         <div className="w-full max-w-2xl mx-auto flex flex-row justify-between items-center px-2">
           
           <div className="flex items-baseline space-x-2">
